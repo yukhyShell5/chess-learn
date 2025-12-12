c8646984da2b1c3b31fb8fb368e23e3ab359361f
shell <zoulou.azerty33@gmail.com>
2025-12-12 10:54:42 +0100

    Add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output:'export',
+  basePath: '/chess-learn',
+  assetPrefix: '/chess-learn/'
 };
 
 export default nextConfig;
