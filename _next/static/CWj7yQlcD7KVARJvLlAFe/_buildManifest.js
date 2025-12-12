84b3d53e5921ffab20587390489e7e080541d2fd
yukhyShell5 <90570322+yukhyShell5@users.noreply.github.com>
2025-12-12 10:02:28 +0000

    Deploying to gh-pages from @ yukhyShell5/chess-learn@e433afc5b3cd0664ffb9d5e6524c133c598323a1 🚀

--- _next/static/CWj7yQlcD7KVARJvLlAFe/_buildManifest.js
+++ _next/static/CWj7yQlcD7KVARJvLlAFe/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/chess-learn//_next/:path+",
-        "destination": "/chess-learn/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
